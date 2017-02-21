feccac4cb974901beab59488abf9d29d5b97d007
liuyanyi <wolfsonliu@163.com>
2017-02-21 23:24:29 +0800

    struct ver

--- calc/calc/calc.cpp
+++ calc/calc/calc.cpp
@@ -1,82 +1,82 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define MAX 10			//定义最大支持的阶数
-void shuchu(float m[MAX][MAX], int hang, int lie)
-//输出矩阵函数，形参为二维数组，行数和列数
+struct Matrix	//矩阵结构体
 {
-	int i, j, k,t;
-	for (i = 0, k = 0; i < hang; i++)	//循环
-		for (j = 0,printf("    | "); j < lie; j++)
+	int row;
+	int column;
+	float m[MAX][MAX];
+};
+void shuchu(struct Matrix *p)
+{
+	int i, j, k, t;
+	for (i = 0, k = 0; i < p->row; i++)	//循环
+		for (j = 0,printf("    | "); j < p->column; j++)
 		{
 			if (k == 0)
 			{
-				for (t = 0; t < lie; t++)
+				for (t = 0; t < p->column; t++)
 					printf("       ");
 				printf(" |\n    | ");
 			}
-			printf("%6.2f ", m[i][j]);	//输出对应的值
+			printf("%6.2f ", p->m[i][j]);	//输出对应的值
 			k++;
-			if (k % lie == 0)
+			if (k % p->column == 0)
 			{
-					printf(" |\n    | ");
-					for (t = 0; t < lie; t++)
-						printf("       ");
-					printf(" |\n");
+				printf(" |\n    | ");
+				for (t = 0; t < p->column; t++)
+					printf("       ");
+				printf(" |\n");
 			}
-			
-			//列数判别，如果k为列数的倍数，则换行
 		}
 	printf("\n");
 }
-float value(float a[MAX][MAX], int n)
-//行列式值计算函数，形参为二维数组和阶数，返回行列式的值
+float value(struct Matrix *p)
 {
 	float sum = 0;
 	int i, j, k, m;
-	float temp[MAX][MAX];		//临时矩阵，用于存放矩阵的余子式
-	switch (n)	//选择阶数
+	struct Matrix temp;
+	struct Matrix *s;
+	s = &temp;
+	temp.column = p->column-1;
+	temp.row = p->row-1;
+	switch (p->column)
 	{
 	case 1:
-		return a[0][0];		//一阶矩阵直接返回其本身
+		return p->m[0][0];
 	case 2:
-		return a[0][0] * a[1][1] - a[0][1] * a[1][0];
-		//二阶矩阵返回对角线元素乘积的差
-	default:	//其余情况
-		for (i = 0; i<n; i++)
-			//循环，将矩阵按第一行的元素展开，取余子式
+		return p->m[0][0] * p->m[1][1] - p->m[0][1] * p->m[1][0];
+	default:
+		for (i = 0; i < p->column; i++)
 		{
-			for (j = 1; j<n; j++)
+			for (j = 1; j < p->column; j++)
 			{
-				m = 0;	//用m来判定代数余子式前所需的正负号
-				for (k = 0; k<n; k++)
+				m = 0;
+				for (k = 0; k < p->column; k++)
 				{
-					if (k != i)		//k不等于i时
-						temp[j - 1][k - m] = a[j][k];
-					//将对应元素赋给新的二维数组，即为余子式
+					if (k != i)	
+						s->m[j - 1][k - m] = p->m[j][k];
 					else m = 1;
 				}
 			}
 			if (i % 2) m = -1;
-			sum += m*a[0][i] * value(temp, n - 1);
-			//用递归计算余子式值并循环加和得到行列式值
+			sum += m*p->m[0][i] * value(s);
 		}
-		return sum;		//返回行列式值
+		return sum;	
 	}
 }
-void swap_line(float m[MAX][MAX], float x[MAX], int n, int n1)
-//换列函数，将n阶矩阵m中的第n1列与x交换
+void swap_line(struct Matrix *p, float x[MAX], int n1)
 {
 	int i;
-	float t[MAX];
-	for (i = 0; i < n; i++)		//交换列
+	float temp[MAX];
+	for (i = 0; i < p->column; i++)		//交换列
 	{
-		t[i] = x[i];
-		x[i] = m[i][n1];
-		m[i][n1] = t[i];
+		temp[i] = x[i];
+		x[i] = p->m[i][n1];
+		p->m[i][n1] = temp[i];
 	}
 }
-float inner_product(float a[MAX], float b[MAX], int n)
-//内积计算函数
+float inner_product(float a[MAX], float b[MAX], int n)	//内积计算函数
 {
 	float ans = 0;
 	int i;
@@ -86,97 +86,115 @@ float inner_product(float a[MAX], float b[MAX], int n)
 }
 void zhuanzhi(void)		//转置部分
 {
-	int i, j, x, y;
-	float a[MAX][MAX], b[MAX][MAX];	//a为原矩阵，b为转置后矩阵
+	int i, j;
+	struct Matrix a[2];
+	struct Matrix *p;
+	p = &a[0];
 	printf("输入矩阵的行数、列数：\n");
-	scanf_s("%d%d", &x, &y);
+	scanf_s("%d%d", &p->row, &p->column);
+	a[1].column = p->row;
+	a[1].row = p->column;
 	printf("输入矩阵：\n");
-	for (i = 0; i < x; i++)		//循环输入矩阵
-		for (j = 0; j < y; j++)
-			scanf_s("%f", &a[i][j]);
-	for (i = 0; i < x; i++)
-		//循环，将将A的i行j列元素赋给B的j行i列
-		for (j = 0; j < y; j++)
-			b[j][i] = a[i][j];
+	for (i = 0; i < p->row; i++)
+		for (j = 0; j < p->column; j++)
+			scanf_s("%f", &p->m[i][j]);
+	for (i = 0; i < p->row; i++)
+		for (j = 0; j < p->column; j++)
+			a[1].m[j][i] = a[0].m[i][j];
 	printf("\n原矩阵为：\n");
-	shuchu(a, x, y);	//输出原矩阵
+	shuchu(p);
 	printf("\n转置矩阵为：\n");
-	shuchu(b, y, x);	//输出转置后矩阵
+	shuchu(p+1);
 }
 void hanglieshi(void)	//行列式部分	
 {
-	int n, i, j;
-	float a[MAX][MAX];
+	struct Matrix a;
+	struct Matrix *p;
+	p = &a;
+	int i, j;
 	printf("输入矩阵阶数：\n");
-	scanf_s("%d", &n);	//输入阶数
+	scanf_s("%d", &p->column);	//输入阶数
+	p->row = p->column;
 	printf("输入矩阵：\n");
-	for (i = 0; i < n; i++)		//循环输入矩阵元素
-		for (j = 0; j < n; j++)
-			scanf_s("%f", &a[i][j]);
+	for (i = 0; i < p->column; i++)		//循环输入矩阵元素
+		for (j = 0; j < p->column; j++)
+			scanf_s("%f", &p->m[i][j]);
 	printf("\n原矩阵为：\n");
-	shuchu(a, n, n);	//调用行列式计算函数计算行列式
-	printf("行列式的值为：%5.2f", value(a, n));
+	shuchu(p);	//调用行列式计算函数计算行列式
+	printf("行列式的值为：%5.2f\n", value(p));
 }
 void jiafa(void)
 {
-	int x, y, i, j, n1, n2;
-	float a[MAX][MAX], b[MAX][MAX], ans[MAX][MAX];
-	//矩阵A，B为原矩阵，ans为结果矩阵
+	int i, j, n1, n2;
+	struct Matrix a[3];
+	struct Matrix *p;
+	p = &a[0];
 	printf("输入矩阵的行数、列数：\n");
-	scanf_s("%d%d", &x, &y);
+	scanf_s("%d%d", &p->row, &p->column);
+	for (p = p + 1, i = 0; i < 3; i++)
+	{
+		p->row = a[0].row;
+		p->column = a[0].column;
+	}
+	p = &a[0];
 	printf("输入A矩阵：\n");
-	for (i = 0; i < x; i++)
-		for (j = 0; j < y; j++)
-			scanf_s("%f", &a[i][j]);	//输入A矩阵
+	for (i = 0; i < p->row; i++)
+		for (j = 0; j < p->column; j++)
+			scanf_s("%f", &p->m[i][j]);	//输入A矩阵
 	printf("输入A的倍数：\n");
 	scanf_s("%d", &n1);
+	p++;
 	printf("输入B矩阵：\n");
-	for (i = 0; i < x; i++)
-		for (j = 0; j < y; j++)
-			scanf_s("%f", &b[i][j]);	//输入B矩阵
+	for (i = 0; i < p->row; i++)
+		for (j = 0; j < p->column; j++)
+			scanf_s("%f", &p->m[i][j]);	//输入B矩阵
 	printf("输入B的倍数：\n");
 	scanf_s("%d", &n2);
-	for (i = 0; i < x; i++)
-		for (j = 0; j < y; j++)
-			ans[i][j] = n1*a[i][j] + n2*b[i][j];	//进行加法运算
+	p++;
+	for (i = 0; i < p->row; i++)
+		for (j = 0; j < p->column; j++)
+			p->m[i][j] = n1*(p - 2)->m[i][j] + n2*(p - 1)->m[i][j];	//进行加法运算
 	printf("%3dA+%3dB = \n", n1, n2);
-	shuchu(ans, x, y);
+	shuchu(p);
 }
 void multiply(void)		//矩阵乘法部分
 {
-	int x1, x2, y1, y2, i, j, k;
-	float a[MAX][MAX], b[MAX][MAX], c[MAX][MAX];
-	//矩阵A，B为原矩阵，C为结果矩阵
+	int i, j, k, t;
+	char ch;
+	struct Matrix a[3];
+	struct Matrix *p;
 	do
-	{		//循环，如果无法进行乘法则重新进行
-		printf("输入A矩阵的行数、列数：\n");
-		scanf_s("%d%d", &x1, &y1);
-		printf("输入B矩阵的行数、列数：\n");
-		scanf_s("%d%d", &x2, &y2);
-		if (y1 != x2) printf("A列数不等于B行数，请重新输入\n");
-	} while (y1 != x2);
-	//若满足该条件，无法进行矩阵乘法，需要重新输入
-	printf("输入A矩阵：\n");
-	for (i = 0; i < x1; i++)
-		for (j = 0; j < y1; j++)
-			scanf_s("%f", &a[i][j]);	//输入A矩阵
-	printf("输入B矩阵：\n");
-	for (i = 0; i < x2; i++)
-		for (j = 0; j < y2; j++)
-			scanf_s("%f", &b[i][j]);	//输入B矩阵
-	for (i = 0; i < x1; i++)
-		for (j = 0; j < y2; j++)
-			c[i][j] = 0;	//将C中全部元素转为0
-	for (i = 0; i < x1; i++)
-		for (j = 0; j < y2; j++)
-			for (k = 0; k < y1; k++)
-				c[i][j] += a[i][k] * b[k][j];	//计算结果赋给C
-	printf("\nA矩阵为：\n");
-	shuchu(a, x1, y1);	//输出A矩阵
-	printf("\nB矩阵为：\n");
-	shuchu(b, x2, y2);		//输出B矩阵
-	printf("\n矩阵AB为：\n");
-	shuchu(c, x1, y2);	//输出C矩阵
+	{
+		for (i = 0, p = &a[0], ch = 'A'; i < 2; i++, ch++, p++)
+		{
+			printf("输入%c矩阵的行数、列数：\n", ch);
+			scanf_s("%d%d", &p->row, &p->column);
+		}
+		if (a[0].column != a[1].row) printf("A列数不等于B行数，请重新输入\n");
+	} while (a[0].column != a[1].row);
+	for (t = 0, p = &a[0], ch = 'A'; t < 2; t++, ch++, p++)
+	{
+		printf("输入%c矩阵：\n", ch);
+		for (i = 0; i < p->row; i++)
+			for (j = 0; j < p->column; j++)
+				scanf_s("%f", &p->m[i][j]);
+	}
+	p++;
+	p->row = a[0].row;
+	p->column = a[0].column;
+	for (i = 0; i < p->row; i++)
+		for (j = 0; j < p->column; j++)
+			p->m[i][j] = 0;
+	for (i = 0; i < p->row; i++)
+		for (j = 0; j < (p-1)->column; j++)
+			for (k = 0; k < p->column; k++)
+				p->m[i][j] += (p - 2)->m[i][k] * (p - 1)->m[k][j];
+	printf("A*B=C\n");
+	for (i = 0, p = &a[0], ch = 'A'; i < 3; i++, p++, ch++)
+	{
+		printf("\n%c矩阵为：\n", ch);
+		shuchu(p);
+	}
 }
 void neiji(void)	//内积部分
 {
@@ -195,30 +213,34 @@ void neiji(void)	//内积部分
 }
 void cramer(void)	//Carmer法解n元线性方程组部分
 {
-	int n, i, j;
-	float a[MAX][MAX], b[MAX], d[MAX], x;	//d为储存结果的数组
+	int i, j;
+	struct Matrix a;
+	struct Matrix *p;
+	p = &a;
+	float b[MAX], d[MAX], x;	//d为储存结果的数组
 	for (i = 0; i < MAX; i++)
 		b[i] = 0;
 	printf("输入未知数的个数：\n");
-	scanf_s("%d", &n);
+	scanf_s("%d", &p->column);
+	p->row = p->column;
 	printf("输入系数矩阵：\n");
-	for (i = 0; i < n; i++)
-		for (j = 0; j < n; j++)
-			scanf_s("%f", &a[i][j]);	//输入矩阵
+	for (i = 0; i < p->column; i++)
+		for (j = 0; j < p->column; j++)
+			scanf_s("%f", &p->m[i][j]);	//输入矩阵
 	printf("输入常数项列：\n");
-	for (i = 0; i < n; i++)
+	for (i = 0; i < p->column; i++)
 		scanf_s("%f", &b[i]);
-	x = value(a, n);	//计算系数矩阵的值x
+	x = value(p);	//计算系数矩阵的值x
 	if (x)	//判断系数矩阵的值，仅在不为零时有唯一解
 	{
-		for (i = 0; i < n; i++)		//循环计算各个未知数的解
+		for (i = 0; i < p->column; i++)		//循环计算各个未知数的解
 		{
-			swap_line(a, b, n, i);	//将常数项列和系数矩阵中的第i列交换
-			d[i] = value(a, n) / x;		//计算未知数x的解
-			swap_line(a, b, n, i);	//将常数项列恢复原状
+			swap_line(p, b, i);	//将常数项列和系数矩阵中的第i列交换
+			d[i] = value(p) / x;		//计算未知数x的解
+			swap_line(p, b, i);	//将常数项列恢复原状
 		}
 		printf("解：\n");
-		for (i = 0; i < n; i++)
+		for (i = 0; i < p->column; i++)
 			printf("x%d=%7.2f\n", i + 1, d[i]);		//输出结果
 	}
 	else printf("该方程无唯一解。\n");	//无解输出
